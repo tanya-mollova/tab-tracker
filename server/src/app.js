12d6ea13ae3ad734843c0bdda0ba7429a3fbe6e9
tanya-mollova <55551633+tanya-mollova@users.noreply.github.com>
2020-11-01 18:52:23 +0200

    express port
    port

--- server/src/app.js
+++ server/src/app.js
@@ -1,15 +1,20 @@
-console.log("hello");
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const morgan  = require('morgan')
+const morgan = require('morgan')
+console.log('helo')
+
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
-app.get('/status',(req,res)=>{
+
+app.post('/register',(req,res)=>{
     res.send({
-        message: 'hello world'
+        message:`Hello ${req.body.email}. Your user was registered. Have fun!`
     })
 })
-app.listen(process.env.PORT || 8081)
+var listener = app.listen(8081, function(){
+    console.log('Listening on port ' + listener.address().port); //Listening on port 8888
+});
+
