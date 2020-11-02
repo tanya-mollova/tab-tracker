0d3129b16183158b414a3500398226c0348344dd
Tanya <tanya@webimpression.net>
2020-11-02 17:27:54 +0200

    server files

---
+++ client/src/components/Register.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <panel>
+        <h1>Register</h1>
+        <form 
+          name="tab-tracker-form"
+          autocomplete="off">
+          <v-text-field
+            label="Email"
+            v-model="email"
+          ></v-text-field>
+          <br>
+          <v-text-field
+            label="Password"
+            type="password"
+            v-model="password"
+            autocomplete="new-password"
+          ></v-text-field>
+        </form>
+        <br>
+        <div class="danger-alert" v-html="error" />
+        <br>
+        <v-btn
+          dark
+          class="cyan"
+          @click="register">
+          Register
+        </v-btn>
+      </panel>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+
+export default {
+  data () {
+    return {
+      email: '',
+      password: '',
+      error: null
+    }
+  },
+  methods: {
+    async register () {
+      try {
+        const response = await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+        
+      } catch (error) {
+        this.error = error.response.data.error
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- server/src/app.js
+++ server/src/app.js
@@ -2,21 +2,18 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
-console.log('test')
+const{sequelize} = require('./models')
+const config = require('./config/config')
 
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
-app.get('/', (req,res) => {
-    res.send('hello')
-})
-app.post('/register',(req,res)=>{
-    res.send({
-        message:`Hello ${req.body.email}. Your user was registered. Have fun!`
-    })
-})
-var listener = app.listen(8081, function(){
-    console.log('Listening on port ' + listener.address().port); 
-});
 
+require('./routes')(app)
+
+sequelize.sync({force: false})
+  .then(() => {
+    app.listen(config.port)
+    console.log(`Server started on port ${config.port}`)
+  })
