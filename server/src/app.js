342c4ff71369a30b7dee5f621606d619cb9070fe
Tanya <tanya@webimpression.net>
2020-11-02 10:55:49 +0200

    register component

--- server/src/app.js
+++ server/src/app.js
@@ -2,19 +2,21 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
-console.log('helo')
+console.log('test')
 
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
-
+app.get('/', (req,res) => {
+    res.send('hello')
+})
 app.post('/register',(req,res)=>{
     res.send({
         message:`Hello ${req.body.email}. Your user was registered. Have fun!`
     })
 })
 var listener = app.listen(8081, function(){
-    console.log('Listening on port ' + listener.address().port); //Listening on port 8888
+    console.log('Listening on port ' + listener.address().port); 
 });
 
